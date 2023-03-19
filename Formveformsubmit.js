51098ae626139db2c874c66ac864f380e7820c34
Ozan Bektas <o.bektas94@gmail.com>
2023-03-19 20:24:03 +0300

    html ve js dosyalari olusturuldu

---
+++ Formveformsubmit.js
@@ -0,0 +1,7 @@
+
+function handleSubmit(event) {
+    event.preventDefault();
+    let isimDOM = document.querySelector("#inputIsim").value
+    let soyisimDOM = document.querySelector("#inputSoyisim").value
+    alert(`Teşekkürler ${isimDOM} ${soyisimDOM}`)
+ }
